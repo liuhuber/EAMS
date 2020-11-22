26fcb3e615d2a7a3c2efadfa94219e9e27db47f8
Huber Liu <liuhuber@hotmail.com>
2020-11-22 02:07:03 +0000

    test1-init

---
+++ test3.php
@@ -0,0 +1,15 @@
+<?php
+
+include "../../mainfile.php";
+include "function.php";
+global $xoopsDB,$xoopsUser;
+
+$main=$_POST[crossteam];
+
+
+include "../../header.php";
+echo "crossteam= $main";
+//echo my_menu();
+
+include "../../footer.php";
+?>
